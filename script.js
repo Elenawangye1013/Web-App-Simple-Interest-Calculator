22ba43709b3a3698c20b69e78e4df0334004ad47
Elenawangye1013 <102157764+Elenawangye1013@users.noreply.github.com>
2022-03-22 15:14:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function compute()
+
+      
+ function actualizarValue(value) {
+     valor = document.getElementById("range").value;
+     val.innerHTML = valor;
+ }     
+
+
+
+function calculate()
 {
-    p = document.getElementById("principal").value;
+    number = document.getElementById("number").value;
+    range = document.getElementById("range").value;
+    years = document.getElementById("years").value;
+    years= parseInt(years);
+   
+ 
+    if(number>0){
+        result.innerHTML = "If you deposit " + number + ", <br>"  +  " at an interest rate of " +range + "% . <br>" +
+        " You will receive an amount of "+ (number*range*years/100) + ", <br>" + " in the year " + (years+2022) + ".";    
+    }
+    else{
+        alert("Please enter a positive number.");
+        location.reload();
+    };
+
+    
+
     
 }
-        
